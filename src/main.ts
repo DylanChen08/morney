515b9e0e74e2c857a091b935ff2ecff650292b7e
DC <easetreychen@gmail.com>
2021-06-05 17:17:02 +0800

    新增Helper动画库 新增增加标签、数字键盘动画

--- src/main.ts
+++ src/main.ts
@@ -6,9 +6,14 @@ import store from './store';
 import Nav from '@/components/Nav.vue';
 import Layout from '@/components/Layout.vue';
 import Icon from '@/components/Icon.vue';
+// @ts-ignore
+import VAnimateCss from 'v-animate-css';
+
 
 Vue.config.productionTip = false;
 
+
+Vue.use(VAnimateCss);
 Vue.component('Nav', Nav);
 Vue.component('Layout', Layout);
 Vue.component('Icon', Icon);
--- src/views/Money.vue
+++ src/views/Money.vue
@@ -3,11 +3,11 @@
     <NumberPad/>
     <Types :xxx='0' />
     <Notes/>
-    <Tags/>
+    <Tags :dataSource.sync="tags"/>
   </Layout>
 </template>
 
-<script lang="ts">
+<script>
   import NumberPad from '@/components/Money/NumberPad.vue';
   import Types from '@/components/Money/Types.vue';
   import Notes from '@/components/Money/Notes.vue';
@@ -15,6 +15,11 @@
 
   export default {
     name: 'Money',
+    data(){
+      return{
+        tags:['衣','食','住','行']
+      }
+    },
     components: {Tags, Notes, Types, NumberPad},
   };
 </script>
