22bf45a4c896076c6c9a0e6b1fafca4eb8e2cb8e
DC <easetreychen@gmail.com>
2021-06-06 22:52:27 +0800

    新增 model.ts

--- src/components/Money/NumberPad.vue
+++ src/components/Money/NumberPad.vue
@@ -71,6 +71,8 @@ export default class NumberPad extends Vue {
 
   ok() {
     this.$emit('update:value', this.output)
+    this.$emit('submit', this.output)
+    this.output = '0'
     console.log(`当前输出的数字为[${this.output}]`)
   }
 
--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,9 +1,9 @@
 <template>
   <Layout class-prefix="layout">
-    {{ record }}
+    {{ recordList }}
     <!-- .sync语法糖-->
     <!-- :value.sync="record.type"===@update:value="onUpdateType" -->
-    <NumberPad :value.sync="record.amount"/>
+    <NumberPad :value.sync="record.amount" @submit="saveRecord"/>
     <Types :value.sync="record.type"/>
     <Notes :value.sync="record.notes"/>
     <Tags :dataSource.sync="tags" @update:value="onUpdateTags"/>
@@ -12,32 +12,46 @@
 
 <script lang="ts">
 import Vue from "vue";
-import {Component} from "vue-property-decorator";
+import {Component, Watch} from "vue-property-decorator";
 import NumberPad from '@/components/Money/NumberPad.vue';
 import Types from '@/components/Money/Types.vue';
 import Notes from '@/components/Money/Notes.vue';
 import Tags from '@/components/Money/Tags.vue';
+import {model} from "@/model";
 
-type Record = {
-  tags: string[]
-  notes: string
-  type: string
-  amount: number
-}
+const recordList= model.fetch()
+// type RecordItem = {
+//   tags: string[]
+//   notes: string
+//   type: string
+//   amount: number //数据类型
+//   createAt?: Date //类/构造函数
+// }
 
 @Component({components: {Tags, Notes, Types, NumberPad},})
 export default class Money extends Vue {
   tags = ['衣', '食', '住', '行'] //为了保证有初始数据，不能和Types、NumberPad和Tags一样只做一个值的传输.
   // record 必须符合 Record 类型
-  record: Record = {
+  recordList: RecordItem[] = recordList
+  record: RecordItem = {
     tags: [], notes: '', type: '-', amount: 999
   }
+
 
   onUpdateTags(value: string[]) {
     this.record.tags = value
   }
 
+  saveRecord() {
+    const item:RecordItem = JSON.parse(JSON.stringify(this.record))
+    item.createAt = new Date()
+    this.recordList.push(item)
+  }
 
+  @Watch('recordList')
+  onRecordListChange() {
+    window.localStorage.setItem('recordList', JSON.stringify(this.recordList))
+  }
 }
 </script>
 
