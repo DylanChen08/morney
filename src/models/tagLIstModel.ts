97a6bc92ee875b02f6591eaefbe87e6e126cd44c
DC <easetreychen@gmail.com>
2021-06-08 16:06:13 +0800

    新增字符段id

--- src/models/tagLIstModel.ts
+++ src/models/tagLIstModel.ts
@@ -1,4 +1,8 @@
 const localStorageKeyName = 'tagList'
+type Tag = {
+    id: string
+    name: string
+}
 type TagListModel = {
     //  string[] => 空字符串
     //  string   => 非空字符串
@@ -14,8 +18,13 @@ const tagListModel: TagListModel = {
         return this.data
     },
     create(name: string) {
-        if (this.data.indexOf(name) >= 0) {return 'duplicated'}
-        this.data.push(name);
+        // @ts-ignore
+        const names = this.data.map(item => item.name);
+        if (names.indexOf(name) >= 0) {
+            return 'duplicated'
+        }
+        // @ts-ignore
+        this.data.push({id: name, name: name});
         this.save();
         return 'success'
     },
---
+++ src/views/EditLabel.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>EditLabel.vue
+    <Layout></Layout>
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: "EditLabel"
+}
+</script>
+
+<style scoped>
+
+</style>
